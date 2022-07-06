24602feae02f9ddc53026559242f50b144d8d9c2
Megaladon <stanko.demon@gmail.com>
2022-07-06 19:55:39 +0300

    my metod

--- OtherWork3/Program.cs
+++ OtherWork3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace OtherWork3
 { 
@@ -7,8 +8,22 @@ namespace OtherWork3
     {
         static void Main(string[] args)
         {
-            int[] array = new int[0];
+            /*
+            1 - Where(на примере данной команды array.Where(number => number != removeNumber).ToArray();) 
+            в number записываются значения из массива и проверяется условие если number 
+            не равен значению из переменной removeNumber то значение записывается, 
+            а если равен то пропускается. ToArray() конвертирует метод (IEnumerable<int>) обратно в массив.
+            Если писать подробно, то напоминает запрос из Базы Данных ((from number in array where number != removeNumber select number).ToArray();)
+            А вот с первой командой я просто неразобрался как реализовать, поэтому написал уже, то что было на одном из дз.
+            2 - Можно задать статический массив и по мере заполнения увеличивать или уменьшать переменную 
+            в которой указанно колличество введенных значений массива.
+            3 - Resize - изменяет размерность элементов до указанной веречины. Служит для реализации динамического массива.
+            4 - ref - указывает на то, что значение передается по ссылке. Необходим для изменения размерности массива на который он указан.
+            */
+
+            int[] array = new int[1000];
             int lenght = 0;
+            ConsoleKeyInfo end;
             string command;
             do
             {
@@ -25,37 +40,48 @@ namespace OtherWork3
                 {
                     case "SetNumbers":
                         Console.WriteLine("Введите массив через пробел");
-                        string stroka = InputString("");
+                        string stroka = InputString("");                       
                         string number = String.Empty;
 
                         for(int i = 0; i < stroka.Length; i++)
                         {
                             if (stroka[i] == ' ')
                             {
-                                AddArray(Convert.ToInt32(number));
+                                array[lenght++] = (Convert.ToInt32(number));
                                 number = String.Empty;
                             }
                             else
                                 number += stroka[i];
                             if (i == stroka.Length - 1)
                             {
-                                AddArray(Convert.ToInt32(number));
+                                array[lenght++] = (Convert.ToInt32(number));
                             }
                         }
                         break;
 
                     case "AddNumbers":
-                        AddArray(InputInteger("Введите число: "));
+                        do
+                        {
+                            array[lenght++] = (InputInteger("Введите число: "));
+                            end = Permission("Желаете продолжить (y/n) ");
+                            Console.WriteLine();
+                        } while (end.Key.ToString() != "N");                       
                         break;
 
                     case "RemoveNumbers":
-                        int removeNumber = InputInteger("Введите число: ");
-                        array = array.Where(number => number != removeNumber).ToArray();
-                        lenght = array.Length;
+                        do
+                        {
+                            int removeNumber = InputInteger("Введите число: ");
+                            for (int i = 0; i < lenght; i++)
+                                if (array[i] == removeNumber)
+                                    RemoveNumbers(i);
+                            end = Permission("Желаете продолжить (y/n) ");
+                            Console.WriteLine();
+                        } while (end.Key.ToString() != "N");                       
                         break;
 
                     case "Numbers":
-                        for (int i = 0; i < array.Length; i++)
+                        for (int i = 0; i < lenght; i++)
                         {
                             Console.Write(array[i] + " ");
                         }
@@ -91,11 +117,27 @@ namespace OtherWork3
                 return int.Parse(Console.ReadLine());
             }
 
-            void AddArray(int number)
+            void RemoveNumbers(int number)
             {
-                Array.Resize(ref array, lenght + 1);
-                array[lenght] = number;
-                lenght++;
+                for(int i = number; i < lenght; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                lenght--;
+            }
+
+            ConsoleKeyInfo Permission(string str)
+            {
+                bool flag = true;
+                Console.Write(str);
+                ConsoleKeyInfo key;
+                do
+                {
+                    key = Console.ReadKey();
+                    if (key.Key.ToString() == "N" || key.Key.ToString() == "Y")
+                        flag = false;
+                } while (flag);
+                return key;
             }
 
             Console.ReadKey();
